dacf5589c62adc92bbbcfbc1dddf60a21cac8fac
Slawomir <zawistowski.slawomir@gmail.com>
2019-02-24 22:28:44 +0100

    Update keyword and title in index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,8 +15,8 @@ class BlogIndex extends React.Component {
     return (
       <Layout location={this.props.location} title={siteTitle}>
         <SEO
-          title="All posts"
-          keywords={[`blog`, `gatsby`, `javascript`, `react`]}
+          title="Strona Główna"
+          keywords={[`psychoterapia`, `Rzeszów`, `depresja`, `poradnia`]}
         />
         <Bio />
         {posts.map(({ node }) => {
