43856335d85eb9bee0e907f070e7e3d29fc638ea
Slawomir <zawistowski.slawomir@gmail.com>
2019-03-10 23:15:22 +0100

    Extract ToggleButton from Menu

--- src/components/menu.js
+++ src/components/menu.js
@@ -4,6 +4,7 @@ import styled from "styled-components"
 import { startsWith } from "../utils/utils"
 
 import Link from "./link"
+import ToggleButton from './toggle'
 
 const NavMenu = styled.nav`
   position: relative;
@@ -137,67 +138,6 @@ const ListLink = props => {
   )
 }
 
-const ToggleButton = styled.div`
-  display: none;
-  position: absolute;
-  top: 1rem;
-  right: 1rem;
-  min-height: 36px;
-  min-width: 36px;
-  cursor: pointer;
-
-  &:hover span {
-    background: ${props => props.headerStyles.fontColorHover};
-  }
-
-  span {
-    display: block;
-    width: 90%;
-    height: 4px;
-    margin: 6px auto 6px;
-    position: relative;
-    
-    background: ${props => props.headerStyles.fontColor};
-    border-radius: 3px;
-    
-    z-index: 1;
-    
-    transition: transform ${props => props.headerStyles.menuTransitions},
-                background ${props => props.headerStyles.menuTransitions},
-                opacity 0.4s ease,
-                width ${props => props.headerStyles.menuTransitions};
-  }
-
-  span:first-child {
-    transform-origin: 90% 50%;
-  }
-
-  span:nth-last-child(1) {
-    transform-origin: 90% 50%;
-  }
-
-  &.toggle-active span {
-    opacity: 1;
-    transform: rotate(-45deg);
-    background: ${props => props.headerStyles.fontColorHover};
-    width: 100%;
-
-    &:nth-child(2) {
-      opacity: 0;
-      transform: rotate(0deg) scale(0.2, 0.2);
-    }
-
-    &:last-child {
-      transform: rotate(45deg);
-      padding-right: 4px;
-    }
-  }
-
-  @media (max-width: ${props => props.headerStyles.menuTreshold}) {
-    display: block;
-  }
-`
-
 class Menu extends React.Component {
   timeoutId;
 
@@ -234,10 +174,6 @@ class Menu extends React.Component {
     let menuClassName = 'menu';
     if (this.state.isOpen) {
       menuClassName += ' menu-active';
-    }
-    let toggleClassName = 'toggle';
-    if (this.state.isOpen) {
-      toggleClassName += ' toggle-active';
     }
 
     let { location, headerStyles } = this.props
@@ -278,14 +214,10 @@ class Menu extends React.Component {
             location={location}>Kontakt</ListLink>
         </ul>
         <ToggleButton
-          className={toggleClassName}
           onClick={this.toggleClickHandler}
           headerStyles={headerStyles}
-        >
-          <span></span>
-          <span></span>
-          <span></span>
-        </ToggleButton>
+          isOpen={this.state.isOpen}
+        />
       </NavMenu>
     )
   }
---
+++ src/components/toggle.js
@@ -0,0 +1,79 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const ToggleWrapper = styled.div`
+  display: none;
+  position: absolute;
+  top: 1rem;
+  right: 1rem;
+  min-height: 36px;
+  min-width: 36px;
+  cursor: pointer;
+
+  &:hover span {
+    background: ${props => props.headerStyles.fontColorHover};
+  }
+
+  span {
+    display: block;
+    width: 90%;
+    height: 4px;
+    margin: 6px auto 6px;
+    position: relative;
+    
+    background: ${props => props.headerStyles.fontColor};
+    border-radius: 3px;
+    
+    z-index: 1;
+    
+    transition: transform ${props => props.headerStyles.menuTransitions},
+                background ${props => props.headerStyles.menuTransitions},
+                opacity 0.4s ease,
+                width ${props => props.headerStyles.menuTransitions};
+  }
+
+  span:first-child {
+    transform-origin: 90% 50%;
+  }
+
+  span:nth-last-child(1) {
+    transform-origin: 90% 50%;
+  }
+
+  &.toggle--active span {
+    opacity: 1;
+    transform: rotate(-45deg);
+    background: ${props => props.headerStyles.fontColorHover};
+    width: 100%;
+
+    &:nth-child(2) {
+      opacity: 0;
+      transform: rotate(0deg) scale(0.2, 0.2);
+    }
+
+    &:last-child {
+      transform: rotate(45deg);
+      padding-right: 4px;
+    }
+  }
+
+  @media (max-width: ${props => props.headerStyles.menuTreshold}) {
+    display: block;
+  }
+`
+
+const ToggleButton = ({ isOpen, onClick, headerStyles }) => {
+  return(
+    <ToggleWrapper
+      className={`toggle ${isOpen ? ' toggle--active' : ''}`}
+      onClick={onClick}
+      headerStyles={headerStyles}
+    >
+      <span></span>
+      <span></span>
+      <span></span>
+    </ToggleWrapper>
+  )
+}
+
+export default ToggleButton
