872a16bed8f2194d883e164a5681fc84ed8e4c9f
Slawomir <zawistowski.slawomir@gmail.com>
2019-04-12 22:10:30 +0200

    Add new page for disorders list

---
+++ src/pages/w-czym-pomagam.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+import Section from '../components/section'
+
+class DisordersIndex extends React.Component {
+  render() {
+    const { data } = this.props
+    const siteTitle = data.site.siteMetadata.title
+
+    return (
+      <Layout location={this.props.location} title={siteTitle} fullWidth>
+        <SEO
+          title="Artykuły"
+          keywords={[`psychoterapia`, `nerwica`, `depresja`, `poradnia`]}
+        />
+        <Section maxWidth="80%">
+          <h1>W czym pomagam</h1>
+        </Section>
+      </Layout>
+    )
+  }
+}
+
+export default DisordersIndex
+
+export const articlesPageQuery = graphql`
+  query DisordersQuery {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
